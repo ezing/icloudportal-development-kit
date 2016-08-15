e7603a67e1eb928202a1773f1664ebc4266b99ff
ljshj <jinshanl@gmail.com>
2016-08-15 17:36:41 +0800

    增加Reflections

--- icloudportal-common/src/main/java/com/icloudportal/common/reflections/Reflections.java
+++ icloudportal-common/src/main/java/com/icloudportal/common/reflections/Reflections.java
@@ -1,25 +1,8 @@
-/**
- * Axelor Business Solutions
- *
- * Copyright (C) 2005-2016 Axelor (<http://axelor.com>).
- *
- * This program is free software: you can redistribute it and/or  modify
- * it under the terms of the GNU Affero General Public License, version 3,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-package com.axelor.common.reflections;
+
+package com.icloudportal.common.reflections;
 
 /**
- * The {@link Reflections} utilities provides fast and easy way to search for
- * resources and types.
+ * {@link Reflections}提供简捷的方法查找资源和类。
  * 
  */
 public final class Reflections {
@@ -28,74 +11,72 @@ public final class Reflections {
 	}
 
 	/**
-	 * Return a {@link ClassFinder} to search for the sub types of the given
-	 * type.
+	 * 返回{@link ClassFinder} 去查找给定类的子类。
 	 * 
 	 * @param <T>
-	 *            the type to search
+	 *            要去查找的类
 	 * @param type
-	 *            the super type
+	 *            父类
 	 * @param loader
-	 *            find with the given {@link ClassLoader}
+	 *            查找所用的{@link ClassLoader}
 	 * 
-	 * @return an instance of {@link ClassFinder}
+	 * @return {@link ClassFinder}实例
 	 */
 	public static <T> ClassFinder<T> findSubTypesOf(Class<T> type, ClassLoader loader) {
 		return new ClassFinder<>(type, loader);
 	}
 
 	/**
-	 * Return a {@link ClassFinder} to search for the sub types of the given
-	 * type.
+	 * 返回{@link ClassFinder} 去查找给定类的子类。
 	 * 
 	 * @param <T>
-	 *            the type to search
+	 *            要去查找的类
 	 * @param type
-	 *            the super type
+	 *            父类
 	 * 
-	 * @return an instance of {@link ClassFinder}
+	 * @return {@link ClassFinder}实例
 	 */
 	public static <T> ClassFinder<T> findSubTypesOf(Class<T> type) {
 		return new ClassFinder<>(type);
 	}
 
 	/**
-	 * Return a {@link ClassFinder} to search for types.
+	 * 返回{@link ClassFinder}去查找类.
 	 * 
 	 * @param loader
-	 *            find with the given {@link ClassLoader}
+	 *            查找所用的{@link ClassLoader}
 	 * 
-	 * @return an instance of {@link ClassFinder}
+	 * @return {@link ClassFinder}实例
 	 */
 	public static ClassFinder<?> findTypes(ClassLoader loader) {
 		return findSubTypesOf(Object.class, loader);
 	}
 
 	/**
-	 * Return a {@link ClassFinder} to search for types.
+	 * 返回查找类的 {@link ClassFinder} 。
 	 * 
-	 * @return an instance of {@link ClassFinder}
+	 * @return  {@link ClassFinder}实例
 	 */
 	public static ClassFinder<?> findTypes() {
 		return findSubTypesOf(Object.class);
 	}
 
 	/**
-	 * Return a {@link ResourceFinder} to search for resources.
+	 * 返回查找资源的{@link ResourceFinder}。
 	 * 
 	 * @param loader
-	 *            find with the given {@link ClassLoader}
+	 *            查找所用的{@link ClassLoader}
 	 * 
-	 * @return an instance of {@link ResourceFinder}
+	 * @return  {@link ResourceFinder}实例
 	 */
 	public static ResourceFinder findResources(ClassLoader loader) {
 		return  new ResourceFinder(loader);
 	}
 
 	/**
-	 * Return a {@link ResourceFinder} to search for resources.
+	 * 返回查找资源的{@link ResourceFinder}。
 	 * 
-	 * @return an instance of {@link ResourceFinder}
+	 * @return {@link ResourceFinder}实例
 	 */
 	public static ResourceFinder findResources() {
 		return new ResourceFinder();
